261f8c1d34060932715066fe074d2a0f013eb7ee
olabadmus <badmus.olatunde@live.com>
2025-09-09 22:16:27 -0400

    Remove AWS profile from provider for CodeBuild compatibility

--- terraform-manifests/c1-versions.tf
+++ terraform-manifests/c1-versions.tf
@@ -22,7 +22,7 @@ terraform {
 # Provider Block
 provider "aws" {
   region  = var.aws_region
-  profile = "default"
+ # profile = "default"
 }
 /*
 Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
